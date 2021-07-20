7d116967bab5f3d234cf78a7d55d537a06144af5
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-20 10:41:16 +0700

    Add project files.

---
+++ SK-Lib-math.h-round-Bagian8__C/Source.c
@@ -0,0 +1,30 @@
+
+#include <math.h>
+#include <stdio.h>
+#include <conio.h>
+
+/*  Source by Microsoft
+    Modified For Learn by Rizky Khapidsyah
+    I.D.E : VS2019 */
+
+int main() {
+    printf("===== Round a float\n\n");
+    float floatValue = 2.4999999f; // float stores a value close to, but not exactly equal to, the initializer below. floatValue will contain 2.5 because it is the closest single precision value
+    printf("roundf(%.1000g) is %.1000g\n", floatValue, roundf(floatValue));
+    printf("roundf(%.1000g) is %.1000g\n", -floatValue, roundf(-floatValue));
+
+    // double stores a value close to, but not exactly equal to, the initializer below. The closest double value is just slightly larger.
+    double doubleValue = 2.4999999;
+    printf("\n===== Round a double\n\n");
+    printf("round(%.1000g) is %.1000g\n", doubleValue, round(doubleValue));
+    printf("round(%.1000g) is %.1000g\n", -doubleValue, round(-doubleValue));
+
+    // long double stores a value close to, but not exactly equal to, the initializer below. The closest long double value is just slightly larger.
+    long double longDoubleValue = 2.4999999L;
+    printf("\n===== Round a long double\n\n");
+    printf("roundl(%.1000g) is %.1000g\n", longDoubleValue, roundl(longDoubleValue));
+    printf("roundl(%.1000g) is %.1000g\n", -longDoubleValue, roundl(-longDoubleValue));
+
+    _getch();
+    return 0;
+}
